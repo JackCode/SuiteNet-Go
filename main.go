5e92eff7fb7b8a9a00cc56aacc2e2db737930bec
J Weber <63063256+JackCode@users.noreply.github.com>
2021-06-22 23:12:55 -0400

    Add POST header req for MainRew

--- main.go
+++ main.go
@@ -19,6 +19,13 @@ func showMaintenanceRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func createMaintenanceRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
 	w.Write([]byte("Create a new maintenance request..."))
 }
 
